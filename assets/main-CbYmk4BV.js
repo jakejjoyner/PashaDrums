6713faec1578fa9cebfffa264445eaac6ba50483
jakejjoyner <jakejjoyner@users.noreply.github.com>
2024-12-31 09:55:54 +0000

    deploy: 5a37b9cdfc84ce58517317f871967b0e944b2d86

---
+++ assets/main-CbYmk4BV.js
@@ -0,0 +1,15 @@
+(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))l(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const o of r.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&l(o)}).observe(document,{childList:!0,subtree:!0});function i(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function l(e){if(e.ep)return;e.ep=!0;const r=i(e);fetch(e.href,r)}})();document.querySelector("#header").innerHTML=`
+    <div id="logo-container">
+      <a href="/">
+        <h2 id="logo" class="julius-sans-one-regular">Pasha Drums</h2>
+      </a>
+    </div>
+    <div id="nav">
+      <ul id="nav-list">
+        <li><a href="/" class="inter-regular" class="home">Home</a></li>
+        <li><a href="/events/" class="inter-regular" class="events">Events</a></li>
+        <li><a href="/media/" class="inter-regular" class="media">Media</a></li>
+        <li><a href="/bio/" class="inter-regular" class="bio">Bio</a></li>
+      </ul>
+    </div>
+`;const n=new IntersectionObserver(s=>{let t=0;s.forEach(i=>{i.isIntersecting&&(setTimeout(()=>{i.target.classList.toggle("animation"),console.log("in view")},t),n.unobserve(i.target))})},{threshold:0});document.querySelectorAll("#fade-in").forEach(s=>{n.observe(s)});
